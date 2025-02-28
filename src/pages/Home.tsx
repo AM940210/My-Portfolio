9e8d452a95d8b4a2745b15d36bc7932d81cfa4cd
ahmad mohammadi <ahmmoh0210@gmail.com>
2025-02-28 23:37:46 +0100

    added an image and wellcoming message.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex flex-col justify-center items-center h-screen text-center bg-gray-50">
+      <img 
+        src="/src/assets/Alogo.jpg" 
+        alt=""
+        className="rounded-full w-48 h-48 shadow-lg mb-4" />
+      <h1 className="text-4xl font-semibold text-gray-800 mb-4">I'am Ahmad Mohammadi, frontend developer.</h1>
+      <p className="text-xl text-gray-600">
+        I'm a passionate Frontend Developer with a love for crafting beautiful and functional web experiences. I specialize in creating responsive, dynamic, and user-friendly interfaces.
+      </p>
+      
     </div>
   );
 };
