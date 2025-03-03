4a149b4a417eec41a48d1de6b298d28aa37fb16e
ahmad mohammadi <ahmmoh0210@gmail.com>
2025-03-03 05:58:07 +0100

    added some icons

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,9 +7,14 @@ const Contact: React.FC = () => {
         <h1 className="text-4xl font-semibold text-gray-800 mb-8">Let's talk</h1>
         <p className="text-lg text-gray-600 mb-8">I'm currently available to take on new projects, so feel free to send me a message about anyting that you want me to work on. You can contact me anytime.</p>
         <p className="text-lg text-gray-600 mb-8">
+        <i className="fas fa-envelope h-6 w-6 text-gray-600 mr-2"></i>
           ahmmoh0210@gmail.com</p>
-        <p className="text-lg text-gray-600 mb-8"><i></i> +46727191174</p>
-        <p className="text-lg text-gray-600"><i></i> Sweden, Gotengborg</p>
+        <p className="text-lg text-gray-600 mb-8">
+        <i className="fas fa-phone-alt h-6 w-6 text-gray-600 mr-2"></i>
+          +46727191174</p>
+        <p className="text-lg text-gray-600">
+        <span className="mr-2"><i className="fas fa-map-marker-alt"></i></span>
+          Sweden, Gotengborg</p>
       </div>
       <div className="w-full md:w-1/2">
         <h1 className="text-4xl font-semibold text-gray-800 mb-6">Get in touch</h1>
